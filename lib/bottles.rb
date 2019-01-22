0d6bcb39b020596e5c0980d69166badf40f14cd1
Leonardo Pistone <lepistone@spotify.com>
2019-01-22 21:48:38 +0100

    Second verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,8 @@
 class Bottles
   def verse(count)
     <<-VERSE
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
+#{count} bottles of beer on the wall, #{count} bottles of beer.
+Take one down and pass it around, #{count-1} bottles of beer on the wall.
 VERSE
   end
 end
