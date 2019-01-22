33f9f62c0e9e1dc32674c54967ce15c67c3fb78a
Leonardo Pistone <lepistone@spotify.com>
2019-01-22 21:53:48 +0100

    Handle a single bottle

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,16 @@
 class Bottles
   def verse(count)
     <<-VERSE
-#{count} bottles of beer on the wall, #{count} bottles of beer.
-Take one down and pass it around, #{count-1} bottles of beer on the wall.
+#{say_bottles count} of beer on the wall, #{say_bottles count} of beer.
+Take one down and pass it around, #{say_bottles count-1} of beer on the wall.
 VERSE
+  end
+
+  def say_bottles(count)
+    if count == 1
+      "1 bottle"
+    else
+      "#{count} bottles"
+    end
   end
 end
