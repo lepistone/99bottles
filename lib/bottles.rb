cfcc51adfc18480de3fa59b22aed2a4f475c5360
Leonardo Pistone <lepistone@spotify.com>
2019-01-22 21:56:56 +0100

    First verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,15 +2,27 @@ class Bottles
   def verse(count)
     <<-VERSE
 #{say_bottles count} of beer on the wall, #{say_bottles count} of beer.
-Take one down and pass it around, #{say_bottles count-1} of beer on the wall.
+#{take_down count} and pass it around, #{say_bottles count-1} of beer on the wall.
 VERSE
   end
 
   def say_bottles(count)
-    if count == 1
+    case count
+    when  0
+      "no more bottles"
+    when 1
       "1 bottle"
     else
       "#{count} bottles"
+    end
+  end
+
+  def take_down(count)
+    case count
+    when 1
+      "Take it down"
+    else
+      "Take one down"
     end
   end
 end
